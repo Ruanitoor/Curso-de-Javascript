845384c55d1a931bc04e637ace05be6b889874d6
Ruanitoor <pardimruan22@gmail.com>
2023-11-14 23:49:30 -0300

    first commit

---
+++ aula1.js
@@ -0,0 +1,6 @@
+//Aula 1
+/*Comentarios através desses dois metodos*/
+
+console.log("assim terá um print dentro do console da pagina html")
+console.log('também posso usar aspas simples, e misturar string com numeros',10,"separando através das virgulas")
+alert('aparece um pop-up com está mensagem')
