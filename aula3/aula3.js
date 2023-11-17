1c2a4bda6492c2f419327e216ab4f08e748ede91
Ruanitoor <pardimruan22@gmail.com>
2023-11-17 00:36:38 -0300

    aula 3 projeto

---
+++ aula3/aula3.js
@@ -0,0 +1,10 @@
+// Number, String, Undefined, Boolean, Null
+
+const nome = 'Ruan'; //String
+const num1 = 67; //Number
+let num2; //Undefined
+const aprovado = true; //Boolean
+const nome2 = null; //Nulo
+
+//const = imutavel, vc define e não pode mudar
+//let = pode mudar
