20db927def7448fe5a3f77cd02befc3e8e35492f
Ruanitoor <pardimruan22@gmail.com>
2023-11-17 00:31:45 -0300

    aula 2 projeto

---
+++ aula-2/aula2.js
@@ -0,0 +1,13 @@
+console.log('Ruan nasceu em 1988');
+console.log('Meu nome é Ruan');
+console.log('Ruan foi ao cinema');
+console.log('Meu amigo Ruan vai viajar hoje.');
+
+// ---------------- //
+
+let nome = 'Ruan';
+
+console.log(nome, 'nasceu em 1988');
+console.log('Meu nome é', nome);
+console.log(nome, 'foi ao cinema');
+console.log('Meu amigo', nome, 'vai viajar hoje.');
